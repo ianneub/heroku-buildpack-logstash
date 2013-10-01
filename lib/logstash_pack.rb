cdd62a22fde13bea70053045b17d608cd0bc790e
Ian Neubert <ian@ianneubert.com>
2013-10-01 11:16:57 -0700

    Set proper output paths during compile time. I hope.

--- lib/logstash_pack.rb
+++ lib/logstash_pack.rb
@@ -2,6 +2,8 @@ require 'yaml'
 
 module LogstashPack
 
+  OUTPUT_PATH = ARGV[0]
+
   LOGSTASH_VERSION = "1.2.1"
   LOGSTASH_URL = "https://download.elasticsearch.org/logstash/logstash/logstash-#{LOGSTASH_VERSION}-flatjar.jar"
 
@@ -11,13 +13,13 @@ module LogstashPack
 
   def self.compile
     log "Downloading Logstash #{LOGSTASH_VERSION}..."
-    `curl #{LOGSTASH_URL} -L --silent -o logstash.jar`
+    `curl #{LOGSTASH_URL} -L --silent -o #{OUTPUT_PATH}/logstash.jar`
   end
 
   def self.release
     {
       "default_process_types" => {
-        "worker" => "/usr/bin/java -server -Xms384M -Xmx384M -Djava.net.preferIPv4Stack=true -XX:+UseParallelOldGC -jar logstash.jar agent -f logstash.conf"
+        "worker" => "/usr/bin/java -server -Xms384M -Xmx384M -Djava.net.preferIPv4Stack=true -XX:+UseParallelOldGC -jar /app/logstash.jar agent -f /app/logstash.conf"
       }
     }.to_yaml
   end
